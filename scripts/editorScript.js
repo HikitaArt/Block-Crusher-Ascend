ed3999f37e10da150faae8053d8cd1764222ec70
HikitaArt <hikitaart2004@gmail.com>
2024-04-20 00:06:12 +0800

    added cursor in editor

--- scripts/editorScript.js
+++ scripts/editorScript.js
@@ -6,9 +6,30 @@ function drawBallPlate(context){
 
     context.fillRect(400-32, 450, 64, 17);
 }
+function clearScreen(context){
+    context.fillStyle = "black";
+    context.fillRect(0, 0, 765, 290);
+    context.fillStyle = "white";
+}
+function selectCell(event, context, rect){
+    let mouseX = 45 + Math.floor(((event.clientX-rect.left)-45)/48)*48;
+    let mouseY = 47 + Math.floor(((event.clientY-rect.top)-47)/27)*27;
+    console.log(mouseX, mouseY);
+    if (mouseX >= 0 && mouseX <= 717 && mouseY >= 21 && mouseY <= 263){
+        clearScreen(context);
+        context.strokeRect(mouseX, mouseY, 38, 17);
+    }
+}
 
 let canvas = document.getElementsByTagName("canvas")[0];
+let screen = canvas.getBoundingClientRect();
 let ctx = canvas.getContext("2d");
 ctx.fillStyle = "white";
+ctx.strokeStyle = "white";
+ctx.lineWidth = 2;
 
 drawBallPlate(ctx);
+
+canvas.onmousemove = function(event){
+    selectCell(event, ctx, screen);
+}
